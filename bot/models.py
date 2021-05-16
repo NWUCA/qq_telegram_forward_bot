f1bd13348981511c38dff936b74f7b24ca44588f
cjc7373 <niuchangcun@gmail.com>
2021-05-16 23:36:18 +0800

    update models

--- bot/models.py
+++ bot/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class User(models.Model):
-    qq_id = models.BigIntegerField(unique=True)
-    telegram_id = models.BigIntegerField(unique=True)
+    qq_id = models.BigIntegerField(unique=True, null=True)
+    telegram_id = models.BigIntegerField(unique=True, null=True)
     qq_nickname = models.TextField()
     telegram_username = models.TextField()
     telegram_name = models.TextField()
@@ -17,8 +17,8 @@ class GroupCard(models.Model):
 
 class Message(models.Model):
     """表示一条消息, 不存储具体内容, 用于回复, 撤回等功能"""
-    message_id_qq = models.BigIntegerField(blank=True, null=True)
-    message_id_tg = models.TextField(blank=True)
+    message_id_qq = models.BigIntegerField(null=True)
+    message_id_tg = models.TextField(null=True)
     qq_group_id = models.BigIntegerField(help_text='QQ 群号')
     deleted = models.BooleanField(default=False, help_text='是否被撤回')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
